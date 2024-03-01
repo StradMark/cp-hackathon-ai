ef39c03a3b435d3b117e25094f48d539e2464f41
Ewoud van Pareren <ewoud@vanparerensoftwaredevelopment.nl>
2024-03-01 14:23:38 +0100

    Fixed query bug introduced by previous commit. More UX improvements.

--- src/App.vue
+++ src/App.vue
@@ -57,15 +57,33 @@
     margin-left: auto;
   }
 
+  .chat-message-error {
+    background-color: coral;
+    color: white;
+    margin-left: auto;
+  }
+
   .chat-message-author {
     display: block;
     font-size: 0.7em;
+    font-weight: bold;
     margin-bottom: 6px;
     opacity: 0.6;
   }
 
-  .chat-message-ai .chat-message-author {
+  .chat-message-ai .chat-message-author,
+  .chat-message-error .chat-message-author {
     text-align: end;
+  }
+
+  @keyframes loading-pulse {
+    0% { opacity: 0.25; }  
+    50% { opacity: 0.6; }
+    100% { opacity: 0.25; }
+  }
+
+  .chat-message-loading {
+    animation: loading-pulse 0.8s infinite alternate;
   }
 
   .chat-input-text {
@@ -77,22 +95,23 @@
 <template>
   <div class="chat-main">
     <div ref="messagesList" class="chat-messages">
-      <template :key="data.query" v-for="data in chatData">
-        <div class="chat-message">
-          <div class="chat-message-author">You</div>
-          <div>{{ data.query }}</div>
+      <template :key="data.message" v-for="data in chatData">
+        <div v-bind:class="data.classes">
+          <div class="chat-message-author">{{ data.author }}</div>
+          <div>{{ data.message }}</div>
         </div>
-        <div class="chat-message chat-message-ai">
-          <div class="chat-message-author">AI</div>
-          <div>{{ data.response }}</div>
+      </template>
+      <template v-if="isAiBusy">
+        <div class="chat-message chat-message-ai chat-message-loading">
+          <div>AI is responding...</div>
         </div>
       </template>
     </div>
 
-    <form @submit="onSubmitQuery($event, query)">
+    <form @submit="onSubmitQuery($event)">
       <div class="chat-prompt">
           <input class="chat-input-text" type="text" v-model="query" />
-          <input type="submit" value="Chat">
+          <input :disabled="isAiBusy" type="submit" value="Chat">
       </div>
     </form>
   </div>
@@ -101,9 +120,11 @@
 <script setup async>
 import { ref } from "vue";
 
-const query = ref('');
 const messagesList = ref(null);
+
+const query = ref('');
 let chatData = ref([]);
+let isAiBusy = ref(false);
 
 const apiKey = process.env.VUE_APP_API_KEY;
 
@@ -113,42 +134,74 @@ const chatModel = new ChatOpenAI({
   openAIApiKey: apiKey
 });
 
-function onSubmitQuery(event, submittedQuery) {
+function onSubmitQuery(event) {
   event.preventDefault();
+
+  if (isAiBusy.value) return;
+
+  let submittedQuery = query.value;
 
   query.value = '';
 
-  if (submittedQuery.startsWith("Test:")) {
+  addChatMessage("query", submittedQuery);
+
+  if (submittedQuery.startsWith("#")) {
+    isAiBusy = true;
     setTimeout(() => {
-      chatData.value.push({
-        query: submittedQuery,
-        response: "Example response"
-      });
-      scrollToBottomOfMessages();
-    }, 150);
+      addChatMessage("response", "Example response");
+      isAiBusy = false;
+    }, 1500);
+
+    return;
   } else {
-    interactWithChat(query);
+    interactWithChat(submittedQuery);
   }
 }
 
-async function interactWithChat(query) {
+async function interactWithChat(submittedQuery) {
+  isAiBusy = true;
   try {
-    const data = await chatModel.invoke(query);
-    chatData.value.push({
-      query: query,
-      response: data.content
-    })
-    scrollToBottomOfMessages();
-    
+    console.log("Query is " + submittedQuery);
+    const data = await chatModel.invoke(submittedQuery);
+    addChatMessage("response", data.content);
+    isAiBusy = false;
     console.log("Response:", data);
   } catch (error) {
+    addChatMessage("error", "Error message");
+    isAiBusy = false;
     console.error("Error:", error);
   }
 }
 
-function scrollToBottomOfMessages() {
-    setTimeout(() => {
-      messagesList.value.scrollTop = messagesList.value.scrollHeight;
-    }, 50);
+function addChatMessage(type, message) {
+  let newMessage = {
+    type: type,
+    message: message,
+    classes: { "chat-message": true },
+    author: ""
+  };
+
+  switch (type) {
+    case "query":
+      newMessage.author = "You";
+      break;
+    case "response":
+      newMessage.author = "AI";
+      newMessage.classes["chat-message-ai"] = true;
+      break;
+    case "error":
+      newMessage.author = "AI";
+      newMessage.classes["chat-message-error"] = true;
+      break;
+    default:
+      throw new Error(`Invalid message type "${type}"`);
+  }
+
+  chatData.value.push(newMessage);
+
+  setTimeout(() => {
+    messagesList.value.scrollTop = messagesList.value.scrollHeight;
+  }, 50);
 }
+
 </script>
